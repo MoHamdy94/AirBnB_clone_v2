8bb0d900fb99e4ad3938ca54079af8679b395de9
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-22 21:55:11 +0200

    commits

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text_changed = text.replace("_", " ")
+    return "Python {}".format(text_changed)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
