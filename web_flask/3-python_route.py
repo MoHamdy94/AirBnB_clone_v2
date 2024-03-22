32d34188eeaabf0ff31876df279405d1f238a111
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-22 21:56:39 +0200

    commit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,6 +26,7 @@ def c_is_fun(text):
     return "C {}".format(text)
 
 
+@app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     text_changed = text.replace("_", " ")
