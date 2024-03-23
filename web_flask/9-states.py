278948a62028400c69f51fc1b457f8fb016be57d
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-23 18:31:18 +0200

    commit

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)    
+    return render_template("9-states.html")
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the database session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
