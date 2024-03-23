6541e3981ebd36d9a6b1cee13ea9a874b66e227b
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-23 17:21:14 +0200

    state list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the database session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0,0.0')
